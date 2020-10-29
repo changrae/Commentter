934a9323f8046dd4555c6d44d30247245c4414b8
changrae <crjang75@gmail.com>
2020-10-29 10:38:04 +0900

    Updates

---
+++ precache-manifest.d8ce0f49497e831827595ca8bbf58d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081a8f9245d272ccfb0d4ca6c7d0fabf",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "aad59f1f23a6a233e635",
+    "url": "/nwitter/static/css/main.a72e0f1a.chunk.css"
+  },
+  {
+    "revision": "78bf867f0d2a716968df",
+    "url": "/nwitter/static/js/2.5a3c28fb.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/nwitter/static/js/2.5a3c28fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad59f1f23a6a233e635",
+    "url": "/nwitter/static/js/main.fbcdba33.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
