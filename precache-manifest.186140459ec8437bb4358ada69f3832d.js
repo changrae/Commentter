249bd358b650a3b5ba6e4910bb98946a88b655a3
James <34081718+changrae@users.noreply.github.com>
2020-11-25 10:27:15 +0900

    Update precache-manifest.186140459ec8437bb4358ada69f3832d.js

--- precache-manifest.186140459ec8437bb4358ada69f3832d.js
+++ precache-manifest.186140459ec8437bb4358ada69f3832d.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "16ed67279b08bc64f3d936ab9cda119e",
-    "url": "/nwitter/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "41cb0990a6c1ca3b6d7b",
-    "url": "/nwitter/static/css/main.a72e0f1a.chunk.css"
+    "url": "/static/css/main.a72e0f1a.chunk.css"
   },
   {
     "revision": "78bf867f0d2a716968df",
-    "url": "/nwitter/static/js/2.5a3c28fb.chunk.js"
+    "url": "/static/js/2.5a3c28fb.chunk.js"
   },
   {
     "revision": "7e320732ce8e269e8d356159c55ade0e",
-    "url": "/nwitter/static/js/2.5a3c28fb.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.5a3c28fb.chunk.js.LICENSE.txt"
   },
   {
     "revision": "41cb0990a6c1ca3b6d7b",
-    "url": "/nwitter/static/js/main.03af4d42.chunk.js"
+    "url": "/static/js/main.03af4d42.chunk.js"
   },
   {
     "revision": "97fb235846154051176c",
-    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+    "url": "/static/js/runtime-main.222ae764.js"
   }
 ]);
