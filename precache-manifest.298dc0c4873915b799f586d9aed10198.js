06f1e87a7e7bf9b1160c0e4a6e7e191f19d7f95c
changrae <crjang75@gmail.com>
2020-11-25 10:06:33 +0900

    Updates

---
+++ precache-manifest.298dc0c4873915b799f586d9aed10198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196abc59639e49555d966eb4a9053fe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41cb0990a6c1ca3b6d7b",
+    "url": "/static/css/main.a72e0f1a.chunk.css"
+  },
+  {
+    "revision": "84e5d7ba02959614efa5",
+    "url": "/static/js/2.e37f8b3a.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/static/js/2.e37f8b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cb0990a6c1ca3b6d7b",
+    "url": "/static/js/main.03af4d42.chunk.js"
+  },
+  {
+    "revision": "bd00b011b89de116e745",
+    "url": "/static/js/runtime-main.85d7b3f1.js"
+  }
+]);
